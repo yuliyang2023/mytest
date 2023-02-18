6f8d063bc822dd3aebfb433038a6c92e549248af
yuliyang <yuliyang@qq.com>
2023-02-18 09:36:04 +0800

    https://github.com/itay-grudev/SingleApplication

--- qt_dmg/quick.cpp
+++ qt_dmg/quick.cpp
@@ -1,8 +1,18 @@
 #include "skeleton.h"
+#include <singleapplication.h>
 
 int main(int argc, char *argv[]) {
-  QApplication app(argc, argv);
+  SingleApplication app(argc, argv);
   Skeleton window;
+  if(app.isSecondary()) {
+    app.sendMessage( app.arguments().join(' ').toUtf8() );
+    return 0;
+  } else {
+    QObject::connect( &app, &SingleApplication::instanceStarted, [ &window ]() {
+      window.raise();
+      window.activateWindow();
+    });
+  }
   window.resize(450, 350);
   window.setWindowTitle("快速命令");
   window.setWindowFlags(Qt::Window);
