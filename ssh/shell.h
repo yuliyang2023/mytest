1e7619c6c3d2ec875b5a627f2f42a267cdd21688
usageek1266 <usageek1266@gmail.com>
2023-02-19 08:34:52 +0800

    ssh gui

--- ssh/shell.h
+++ ssh/shell.h
@@ -1,32 +1,8 @@
-/****************************************************************************
-**
-** Copyright (C) 2016 The Qt Company Ltd.
-** Contact: https://www.qt.io/licensing/
-**
-** This file is part of Qt Creator.
-**
-** Commercial License Usage
-** Licensees holding valid commercial Qt licenses may use this file in
-** accordance with the commercial license agreement provided with the
-** Software or, alternatively, in accordance with the terms contained in
-** a written agreement between you and The Qt Company. For licensing terms
-** and conditions see https://www.qt.io/terms-conditions. For further
-** information use the contact form at https://www.qt.io/contact-us.
-**
-** GNU General Public License Usage
-** Alternatively, this file may be used under the terms of the GNU
-** General Public License version 3 as published by the Free Software
-** Foundation with exceptions as appearing in the file LICENSE.GPL3-EXCEPT
-** included in the packaging of this file. Please review the following
-** information to ensure the GNU General Public License requirements will
-** be met: https://www.gnu.org/licenses/gpl-3.0.html.
-**
-****************************************************************************/
-
 #pragma once
-
 #include <QObject>
 #include <QSharedPointer>
+#include <QMainWindow>
+#include "console.h"
 
 namespace QSsh {
 class SshConnection;
@@ -40,16 +16,15 @@ class QFile;
 class QString;
 QT_END_NAMESPACE
 
-class Shell : public QObject
+class Shell : public QMainWindow
 {
     Q_OBJECT
 public:
-    Shell(const QSsh::SshConnectionParameters &parameters, QObject *parent = 0);
+    explicit Shell(QWidget *parent = nullptr);
     ~Shell();
 
-    void run();
-
 private:
+    Console *m_console = nullptr;
     void handleConnected();
     void handleConnectionError();
     void handleRemoteStdout();
@@ -57,9 +32,8 @@ private:
     void handleShellMessage(const QString &message);
     void handleChannelClosed(int exitStatus);
     void handleShellStarted();
-    void handleStdin();
+    void writeData(const QByteArray &data);
 
     QSsh::SshConnection *m_connection;
     QSharedPointer<QSsh::SshRemoteProcess> m_shell;
-    QFile * const m_stdin;
 };
